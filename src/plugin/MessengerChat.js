04e375acc2a7cc61d8d158e158fa0442475c2c07
Dara Adenipekun <daraadenipekun97@gmail.com>
2023-07-01 15:14:10 +0100

    facebook added dialog fix

--- src/plugin/MessengerChat.js
+++ src/plugin/MessengerChat.js
@@ -39,7 +39,7 @@ function MessengerChat() {
       setShow(false)
       console.log("show is false")
       window.setTimeout(() => {
-        window.FB.CustomerChat.showDialog()        
+        window.FB.CustomerChat.show()
         console.log("Delayed function called after 5 seconds");
       }, 5000);
      }
